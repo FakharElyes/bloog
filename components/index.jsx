ae1b4d3b32016bad4fa0fe98a069366c1eedd50c
FakharElyes <elyes.fakhar@esprit.tn>
2022-06-08 17:20:46 +0100

    featured posts

--- components/index.jsx
+++ components/index.jsx
@@ -5,7 +5,7 @@ export { default as Categories } from './Categories';
 export { default as Author } from './Author';
 export { default as PostWidget } from './PostWidget';
 // export { default as AdjacentPostCard } from './AdjacentPostCard';
-// export { default as FeaturedPostCard } from './FeaturedPostCard';
+export { default as FeaturedPostCard } from './FeaturedPostCard';
 export { default as Comments } from './Comments';
 export { default as CommentsForm } from './CommentsForm';
 // export { default as Loader } from './Loader';
