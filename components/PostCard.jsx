328029ce31ceaadbd0045747a4aa2aa62f4bbd51
FakharElyes <elyes.fakhar@esprit.tn>
2022-06-08 17:30:48 +0100

    add loader

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -3,6 +3,8 @@ import moment from 'moment';
 import Link from 'next/link';
 import Image from 'next/image';
 
+import { grpahCMSImageLoader } from '../util';
+
 const PostCard = ({ post }) => {
   return (
     <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
@@ -20,7 +22,7 @@ const PostCard = ({ post }) => {
         <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8 items-center">
           <Image
             unoptimized
-            // loader={grpahCMSImageLoader}
+            loader={grpahCMSImageLoader}
             alt={post.author.name}
             height="30px"
             width="30px"
