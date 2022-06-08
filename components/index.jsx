a6185fefc9b4fd4250acb3cbe34bec173921ec2e
FakharElyes <elyes.fakhar@esprit.tn>
2022-06-08 12:42:26 +0100

    header

--- components/index.jsx
+++ components/index.jsx
@@ -1,6 +1,6 @@
 export { default as PostCard } from './PostCard';
 // export { default as PostDetail } from './PostDetail';
-// export { default as Layout } from './Layout';
+export { default as Layout } from './Layout';
 export { default as Categories } from './Categories';
 // export { default as Author } from './Author';
 export { default as PostWidget } from './PostWidget';
@@ -9,3 +9,6 @@ export { default as PostWidget } from './PostWidget';
 // export { default as Comments } from './Comments';
 // export { default as CommentsForm } from './CommentsForm';
 // export { default as Loader } from './Loader';
+
+
+export { default as Header } from './Header';
